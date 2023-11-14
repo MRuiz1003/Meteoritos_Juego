970786a08b8ae6487310925b253c315cda178b4a
MRuiz1003 <marcosezeruiz@gmail.com>
2023-11-14 20:46:44 -0300

    Creacion meteorito y spawner
    Se creó la escena meteorito y su spawner

--- Juego/Niveles/Nivel.gd
+++ Juego/Niveles/Nivel.gd
@@ -2,8 +2,10 @@ class_name Nivel
 extends Node2D
 
 export var explosion: PackedScene = null
+export var meteorito: PackedScene = preload("res://Juego/Meteoritos/Meteorito.tscn")
 
 onready var contenedor_proyectiles: Node
+onready var contenedor_meteoritos: Node
 
 func _ready() -> void:
 	conectar_seniales()
@@ -12,11 +14,23 @@ func _ready() -> void:
 func conectar_seniales() -> void:
 	Eventos.connect("disparo", self, "_on_disparo")
 	Eventos.connect("nave_destruida", self, "_on_nave_destruida")
+	Eventos.connect("spawn_meteorito", self, "_on_spawn_meteoritos")
 	
 func crear_contenedores() -> void:
 	contenedor_proyectiles = Node.new()
 	contenedor_proyectiles.name = "ContenedorProyectiles"
 	add_child(contenedor_proyectiles)
+	contenedor_meteoritos = Node.new()
+	contenedor_meteoritos.name = "ContenedorMeteoritos"
+	add_child(contenedor_meteoritos)
+	
+func _on_spawn_meteoritos( pos_spawn: Vector2, dir_meteorito: Vector2) -> void:
+	var new_meteorito: Meteorito = meteorito.instance()
+	new_meteorito.crear(
+		pos_spawn,
+		dir_meteorito
+	)
+	contenedor_meteoritos.add_child(new_meteorito)
 
 func _on_disparo(proyectil: Proyectil) -> void:
 	contenedor_proyectiles.add_child(proyectil)
