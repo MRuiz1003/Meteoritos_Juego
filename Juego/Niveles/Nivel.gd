bacf7162354c0e0d71dcc3659d79aeca2051214c
MRuiz1003 <marcosezeruiz@gmail.com>
2023-11-21 01:55:22 -0300

    Base enemiga v1
    Se agregó la base enemiga, con 4 animaciones aleatorias. Puede atraer al jugador, ser destruida y destruir al jugador si este entra en contacto con ella

--- Juego/Niveles/Nivel.gd
+++ Juego/Niveles/Nivel.gd
@@ -36,6 +36,7 @@ func conectar_seniales() -> void:
 	Eventos.connect("meteorito_destruido", self, "_on_meteorito_destruido")
 # warning-ignore:return_value_discarded
 	Eventos.connect("nave_en_sector_peligro", self, "_on_nave_en_sector_peligro")
+	Eventos.connect("base_destruida", self, "_on_base_destruida")
 	
 func crear_contenedores() -> void:
 	## Disparos
@@ -132,15 +133,12 @@ func _on_nave_destruida(nave: Player, posicion: Vector2, num_explosiones: int) -
 	if nave is Player:
 		transicion_camaras(
 			posicion,
-			posicion + crear_posicion_aleatoria(-200.0, -200.0),
+			posicion + crear_posicion_aleatoria(200.0, 200.0),
 			camara_nivel,
 			tiempo_transicion_camara
 		)
-	for _i in range(num_explosiones):
-		var new_explosion: Node2D = explosion.instance()
-		new_explosion.global_position = posicion + crear_posicion_aleatoria(100.0, 50.0)
-		add_child(new_explosion)
-		yield(get_tree().create_timer(0.6), "timeout")
+	crear_explosion(posicion, num_explosiones, 0.6, Vector2(100.0, 50.0))
+	
 		
 func _on_meteorito_destruido(pos: Vector2) -> void:
 	var new_explosion: ExplosionMeteorito = explosion_meteorito.instance()
@@ -158,3 +156,23 @@ func _on_nave_en_sector_peligro(centro_cam: Vector2, tipo_peligro: String, num_p
 func _on_TweenCamara_tween_completed(object: Object, _key: NodePath) -> void:
 	if object.name == "CamaraPlayer":
 		object.global_position = $Player.global_position
+		
+func _on_base_destruida(pos_partes: Array) -> void:
+	for posicion in pos_partes:
+		crear_explosion(posicion)
+		yield(get_tree().create_timer(0.5), "timeout")
+	
+func crear_explosion(
+		posicion: Vector2,
+		numero: int = 1,
+		intervalo: float = 0.0,
+		rangos_aleatorios: Vector2 = Vector2(0.0, 0.0)
+		) -> void:
+			for _i in range(numero):
+				var new_explosion: Node2D = explosion.instance()
+				new_explosion.global_position = posicion + crear_posicion_aleatoria(
+					rangos_aleatorios.x,
+					rangos_aleatorios.y
+				)
+				add_child(new_explosion)
+				yield(get_tree().create_timer(0.6), "timeout")
